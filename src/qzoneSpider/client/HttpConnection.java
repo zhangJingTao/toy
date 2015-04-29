e7d35eb70e794f63dbd57ed8e9fde7535d09a753
张京涛 <zhangjingtaosleep@163.com>
2015-04-29 18:18:02 +0800

    对接qq接口
    目前状态：未模拟qq登录，获取数据失败。

---
+++ src/qzoneSpider/client/HttpConnection.java
@@ -0,0 +1,75 @@
+package client;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class HttpConnection{
+
+
+    /**
+     * 发送Get请求
+     * @param url
+     * @return
+     * @throws IOException
+     * @throws JSONException
+     */
+    public static JSONObject readContentFromGet(String url) throws IOException, JSONException {
+        String getURL = url;
+        URL getUrl = new URL(getURL);
+        HttpURLConnection connection = (HttpURLConnection) getUrl
+                .openConnection();
+        connection.connect();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(),"utf-8"));//设置编码,否则中文乱码
+        String lines;
+        StringBuffer html = new StringBuffer();
+        while ((lines = reader.readLine()) != null){
+           html.append(lines);
+        }
+        reader.close();
+        connection.disconnect();
+        return new JSONObject(html.toString());
+    }
+
+    /**
+     * 发送Post请求
+     * @param url
+     * @throws IOException
+     * @throws JSONException
+     */
+    public static JSONObject readContentFromPost(String url) throws IOException, JSONException {
+        URL postUrl = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) postUrl
+                .openConnection();
+        connection.setDoOutput(true);
+        connection.setDoInput(true);
+        connection.setRequestMethod("POST");
+        connection.setUseCaches(false);
+        connection.setInstanceFollowRedirects(true);
+        connection.setRequestProperty("Content-Type",
+                "application/x-www-form-urlencoded");
+        connection.connect();
+        DataOutputStream out = new DataOutputStream(connection
+                .getOutputStream());
+        String content = "" + "&activatecode=" + URLEncoder.encode("久酷博客", "utf-8");
+        out.writeBytes(content);
+        out.flush();
+        out.close(); // flush and close
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(),"utf-8"));//设置编码,否则中文乱码
+        String line;
+        StringBuffer html = new StringBuffer();
+        while ((line = reader.readLine()) != null){
+            html.append(line);
+        }
+        reader.close();
+        connection.disconnect();
+        return new JSONObject(html.toString());
+    }
+}
--- src/qzoneSpider/config/Config.java
+++ src/qzoneSpider/config/Config.java
@@ -5,9 +5,7 @@ package config;
  * Created by ZhangJingtao on 2015/4/28.
  */
 public class Config {
-    public static int REPEAT_TIMES = 3;//尝试错误次数
-    public static String URL = "";
-    public static Integer PAGES = 0;//获取页数 0 == 获取所有
+    public static String URL = "http://taotao.qq.com/cgi-bin/emotion_cgi_msglist_v6?uin=[QQ]&inCharset=utf-8&outCharset=utf-8&hostUin=34572806&notice=0&sort=0&pos=[POS]&num=20&code_version=1&format=json&need_private_comment=1&g_tk=910822701";
     //    static String DB_DRIVER = "com.mysql.jdbc.Driver";
 //    static String DB_URL = "jdbc:mysql://localhost:3306/toy?useUnicode=true&characterEncoding=utf-8";//
     public static String DB_URL = "localhost";
